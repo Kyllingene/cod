86139be10acf68b9b16afde4b4b9c3bba75d61ea
Kyllingene <fuzzymuffin343@gmail.com>
2023-10-29 20:58:08 -0700

    Added clear::line

--- src/lib.rs
+++ src/lib.rs
@@ -102,12 +102,19 @@ pub mod color {
 
 /// Utilities for clearing the screen.
 pub mod clear {
+    use crate::escape;
+
     /// Clear the screen (full clear, not scroll).
     pub fn all() {
-        print!("{}c", 27 as char);
+        escape("2J");
     }
 
-    /// Clear a portion of the screen.
+    /// Clear the current line.
+    pub fn line() {
+        escape("2K");
+    }
+
+    /// Clear a portion of the screen. *Note:* will clear using the current background color.
     pub fn rect(x1: u32, y1: u32, x2: u32, y2: u32) -> Result<(), crate::CodError> {
         crate::rect(' ', x1, y1, x2, y2)
     }
