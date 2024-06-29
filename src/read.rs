3eb550cc79078af974251ac80cb57e91272af100
Kyllingene <fuzzymuffin@gmail.com>
2024-06-29 00:14:10 -0700

    Added reexport for KeyState

--- src/read.rs
+++ src/read.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::must_use_candidate)]
 
 use crossterm::event::{Event, KeyEventKind};
-pub use crossterm::event::{KeyCode, KeyEvent};
+pub use crossterm::event::{KeyCode, KeyEvent, KeyState};
 
 /// Read a single key from stdin.
 pub fn key() -> Option<KeyEvent> {
